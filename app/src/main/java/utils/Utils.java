6dbc0a71778ae56b19a63194a069c195c2b58e03
SingleCycle <zhaoqiwang@hustunique.com>
2014-11-03 11:05:53 +0800

    add Button

--- app/src/main/java/utils/Utils.java
+++ app/src/main/java/utils/Utils.java
@@ -2,13 +2,35 @@ package utils;
 
 import android.content.res.Resources;
 import android.util.TypedValue;
+import android.view.View;
 
 /**
  * Created by singlecycle on 10/30/14.
  */
 public class Utils {
+    /**
+     * 将dp转化为px
+     *
+     * @param dp
+     * @param resources
+     * @return
+     */
+    public static int dpToPx(float dp, Resources resources) {
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics());
+        return (int) px;
+    }
 
-    public static float dpToPx(float dp, Resources resources) {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics());
+    public static int getRelativeTop(View myView) {
+        if (myView.getId() == android.R.id.content)
+            return myView.getTop();
+        else
+            return myView.getTop() + getRelativeTop((View) myView.getParent());
+    }
+
+    public static int getRelativeLeft(View myView) {
+        if (myView.getId() == android.R.id.content)
+            return myView.getLeft();
+        else
+            return myView.getLeft() + getRelativeLeft((View) myView.getParent());
     }
 }
